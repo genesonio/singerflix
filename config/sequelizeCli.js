84cf2b20190bc30244a1664849170e44d8c348ed
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2022-09-18 10:58:55 -0300

    First commit

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    post: '5432',
+    database: 'singerflix_development',
+    username: 'singerflix',
+    password: 'singerflix'
+  }
+}
